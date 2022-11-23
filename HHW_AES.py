d3e79d08d5d756378314bb1370853111e89c1219
RaffaeleParadiso <raffaele05@gmail.com>
2022-11-23 18:52:56 +0100

    AES

---
+++ HHW_AES.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from configuration import (EUOptionPriceFromMCPathsGeneralizedStochIR, CP, NPaths,
+                            NSteps, lambd, eta, S0, T, K, P0T, gamma, vbar, v0,
+                            rhoxr, rhoxv, kappa)
+
+def CIR_Sample(NPaths,kappa,gamma,vbar,s,t,v_s):
+    delta = 4.0 *kappa*vbar/gamma/gamma
+    c= 1.0/(4.0*kappa)*gamma*gamma*(1.0-np.exp(-kappa*(t-s)))
+    kappaBar = 4.0*kappa*v_s*np.exp(-kappa*(t-s))/(gamma*gamma*(1.0-np.exp(-kappa*(t-s))))
+    sample = c* np.random.noncentral_chisquare(delta,kappaBar,NPaths)
+    return  sample
+
+def GeneratePathsHestonHW_AES(NPaths,NSteps,P0T,T,S_0,kappa,gamma,rhoxr,rhoxv,vbar,v0,lambd,eta):
+    dt = 0.0001
+    f0T = lambda t: - (np.log(P0T(t+dt))-np.log(P0T(t-dt)))/(2*dt)
+    # Initial interest rate is forward rate at time t->0
+    r0 = f0T(0.00001)
+    theta = lambda t: 1.0/lambd * (f0T(t+dt)-f0T(t-dt))/(2.0*dt) + f0T(t) + eta**2/(2.0*lambd**2)*(1.0-np.exp(-2.0*lambd*t))
+    Z1 = np.random.normal(0.0,1.0,[NPaths,NSteps])
+    Z2 = np.random.normal(0.0,1.0,[NPaths,NSteps])
+    Z3 = np.random.normal(0.0,1.0,[NPaths,NSteps])
+    W1 = np.zeros([NPaths, NSteps+1])
+    W2 = np.zeros([NPaths, NSteps+1])
+    W3 = np.zeros([NPaths, NSteps+1])
+    V = np.zeros([NPaths, NSteps+1])
+    X = np.zeros([NPaths, NSteps+1])
+    R = np.zeros([NPaths, NSteps+1])
+    M_t = np.ones([NPaths,NSteps+1])
+    R[:,0]=r0
+    V[:,0]=v0
+    X[:,0]=np.log(S_0)
+    time = np.zeros([NSteps+1])
+    dt = T / float(NSteps)
+    for i in range(0,NSteps):
+        # Making sure that samples from a normal have mean 0 and variance 1
+        if NPaths > 1:
+            Z1[:,i] = (Z1[:,i] - np.mean(Z1[:,i])) / np.std(Z1[:,i])
+            Z2[:,i] = (Z2[:,i] - np.mean(Z2[:,i])) / np.std(Z2[:,i])
+            Z3[:,i] = (Z3[:,i] - np.mean(Z3[:,i])) / np.std(Z3[:,i])
+        W1[:,i+1] = W1[:,i] + np.power(dt, 0.5)*Z1[:,i]
+        W2[:,i+1] = W2[:,i] + np.power(dt, 0.5)*Z2[:,i]
+        W3[:,i+1] = W3[:,i] + np.power(dt, 0.5)*Z3[:,i]
+        R[:,i+1] = R[:,i] + lambd*(theta(time[i]) - R[:,i]) * dt + eta* (W1[:,i+1]-W1[:,i])
+        M_t[:,i+1] = M_t[:,i] * np.exp(0.5*(R[:,i+1] + R[:,i])*dt)
+        # Exact samples for the variance process
+        V[:,i+1] = CIR_Sample(NPaths,kappa,gamma,vbar,0,dt,V[:,i])
+        k0 = -rhoxv /gamma * kappa*vbar*dt
+        k2 = rhoxv/gamma
+        k1 = kappa*k2 -0.5
+        k3 = np.sqrt(1.0-rhoxr*rhoxr - rhoxv*rhoxv)
+        X[:,i+1] = X[:,i] + k0 + (k1*dt - k2)*V[:,i] + R[:,i]*dt + k2*V[:,i+1] + np.sqrt(V[:,i]*dt)*(rhoxr*Z1[:,i] + k3 * Z3[:,i])
+        # Moment matching component, i.e. ensure that E(S(T)/M(T))= S0
+        a = S_0 / np.mean(np.exp(X[:,i+1])/M_t[:,i+1])
+        X[:,i+1] = X[:,i+1] + np.log(a)
+        time[i+1] = time[i] +dt
+    # Compute exponent
+    S = np.exp(X)
+    paths = {"time":time,"S":S,"R":R,"M_t":M_t}
+    return paths
+
+np.random.seed(1)
+pathsExact = GeneratePathsHestonHW_AES(NPaths,NSteps,P0T,T,S0,kappa,gamma,rhoxr,rhoxv,vbar,v0,lambd,eta)
+S_ex = pathsExact["S"]
+M_t_ex = pathsExact["M_t"]
+print(np.mean(S_ex[:,1]/M_t_ex[:,1]))
+print(np.mean(S_ex[:,-1]/M_t_ex[:,-1]))
+valueOptMC_ex= EUOptionPriceFromMCPathsGeneralizedStochIR(CP,S_ex[:,-1],K,M_t_ex[:,-1])
+#==============================================================================
+plt.figure(1)
+plt.plot(K,valueOptMC_ex,'.k')
+plt.ylim([0.0,110.0])
+plt.legend(['AES'])
+plt.xlabel('Strike, K')
+plt.ylabel('EU Option Value')
+plt.grid()
+# plt.savefig("img/MC_vs_AES_vs_COS.png",bbox_inches='tight')
+plt.show()
