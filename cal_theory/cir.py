a1c7e58e7db81b887fd53e7df689283869547722
RaffaeleParadiso <raffaele05@gmail.com>
2023-03-07 17:42:18 +0100

    set params for Feller cond.

--- cal_theory/cir.py
+++ cal_theory/cir.py
@@ -21,8 +21,7 @@ def CIREuler(NPaths,NSteps,T,kappa,v0,vbar,gamma):
         V[:,i+1] = V[:,i] + kappa*(vbar - V[:,i]) * dt + gamma* np.sqrt(V[:,i]) * (W[:,i+1]-W[:,i])
         V[:,i+1] = np.maximum(V[:,i+1],0.0)  # Truncation scheme for negative values (maximum)
         time[i+1] = time[i] + dt
-    paths = {"time":time,"V":V}
-    return paths
+    return time, V
 
 def meanSqrtV_1(kappa,v0,vbar,gamma):
     """
@@ -32,8 +31,7 @@ def meanSqrtV_1(kappa,v0,vbar,gamma):
     delta = (4.0*kappa*vbar)/(gamma**2)
     c = lambda t: (gamma**2)*(1.0-np.exp(-kappa*t))/(4.0*kappa)
     kappaBar = lambda t: 4.0*kappa*v0*np.exp(-kappa*t)/((gamma**2)*(1.0-np.exp(-kappa*t)))
-    result = lambda t: np.sqrt(c(t) *((kappaBar(t)-1.0) + delta + delta/(2.0*(delta + kappaBar(t)))))
-    return result
+    return lambda t: np.sqrt(c(t) *((kappaBar(t)-1.0) + delta + delta/(2.0*(delta + kappaBar(t)))))
 
 def meanSqrtV_2(kappa,v0,vbar,gamma):
     """
@@ -50,37 +48,58 @@ def meanSqrtV_2(kappa,v0,vbar,gamma):
 
 if __name__ == "__main__":
 
+    import matplotlib.pyplot as plt
+    from matplotlib import pylab
+    from pylab import *
+    pylab.rcParams['figure.figsize'] = (13, 4)
+
     NPaths = 10000
-    NSteps = 200
-    T      = 5.0
+    NSteps = 2000
+    T      = 100.0
+    time2   = np.linspace(0,T,10)
 
-    Parameters1 ={"kappa":0.8,"gamma":0.1,"v0":0.03,"vbar":0.04}
-    # Parameters2 ={"kappa":1.2,"gamma":0.1,"v0":0.035,"vbar":0.02}
-    # Parameters3 ={"kappa":1.2,"gamma":0.2,"v0":0.05,"vbar":0.02}
-    # Parameters4 ={"kappa":0.8,"gamma":0.25,"v0":0.15,"vbar":0.1}
-    # Parameters5 ={"kappa":1.0,"gamma":0.2,"v0":0.11,"vbar":0.06}
+    SAVE = 0
 
-    ParV = [Parameters1]#, Parameters2, Parameters3, Parameters4, Parameters5]
+    # Feller Condition 2* kappa * vbar > gamma^2
+    Parameters1 ={"kappa":1.2,"gamma":0.1,"v0":0.04,"vbar":0.03} # yep
+    Parameters2 ={"kappa":1.2,"gamma":0.22,"v0":0.035,"vbar":0.02} # nope
 
     plt.figure()
-    for par in ParV:
-        kappa   = par["kappa"]
-        gamma   = par["gamma"]
-        v0      = par["v0"]
-        vbar    = par["vbar"]
-        PathsVolHeston = CIREuler(NPaths,NSteps,T,kappa,v0,vbar,gamma)
-        time    = PathsVolHeston["time"]
-        time2   = np.linspace(0.0,T,20)
-        V       = PathsVolHeston["V"]
-        Vsqrt   = np.sqrt(V)
-        EsqrtV  = Vsqrt.mean(axis=0)
-        plt.plot(time,EsqrtV,'x',markersize=1)
-        approx1 = meanSqrtV_1(kappa,v0,vbar,gamma)
-        approx2 = meanSqrtV_2(kappa,v0,vbar,gamma)
-        plt.plot(time,approx1(time),'--r', label=f'first approx')
-        plt.plot(time2,approx2(time2),'.k', label=f"second approx")
+    kappa   = Parameters1["kappa"]
+    gamma   = Parameters1["gamma"]
+    v0      = Parameters1["v0"]
+    vbar    = Parameters1["vbar"]
+    time, V = CIREuler(NPaths,NSteps,T,kappa,v0,vbar,gamma)
+    Vsqrt   = np.sqrt(V)
+    EsqrtV  = Vsqrt.mean(axis=0)
+    plt.plot(time,EsqrtV,'x',markersize=2, label="Exact")
+    approx1 = meanSqrtV_1(kappa,v0,vbar,gamma)
+    approx2 = meanSqrtV_2(kappa,v0,vbar,gamma)
+    plt.plot(time,approx1(time),'--r', label=f'First Approx')
+    plt.plot(time2,approx2(time2),'.k', label=f"Second Approx")
     plt.xlabel('time')
     plt.ylabel(r'$\mathbb{E}[\sqrt{v_t}]$', size=15)
     plt.legend()
     plt.grid()
+    if SAVE: plt.savefig("HullWhitelambda.png",bbox_inches='tight')
+    plt.show()
+
+    plt.figure()
+    kappa   = Parameters2["kappa"]
+    gamma   = Parameters2["gamma"]
+    v0      = Parameters2["v0"]
+    vbar    = Parameters2["vbar"]
+    time, V = CIREuler(NPaths,NSteps,T,kappa,v0,vbar,gamma)
+    Vsqrt   = np.sqrt(V)
+    EsqrtV  = Vsqrt.mean(axis=0)
+    plt.plot(time,EsqrtV,'x',markersize=2, label="Exact")
+    approx1 = meanSqrtV_1(kappa,v0,vbar,gamma)
+    approx2 = meanSqrtV_2(kappa,v0,vbar,gamma)
+    plt.plot(time,approx1(time),'--r', label=f'First Approx')
+    plt.plot(time2,approx2(time2),'.k', label=f"Second Approx")
+    plt.xlabel('time')
+    plt.ylabel(r'$\mathbb{E}[\sqrt{v_t}]$', size=15)
+    plt.legend()
+    plt.grid()
+    if SAVE: plt.savefig("HullWhitelambda.png",bbox_inches='tight')
     plt.show()
