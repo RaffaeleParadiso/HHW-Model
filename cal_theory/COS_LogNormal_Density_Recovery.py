c0c5f62ab3e4fc4ac512ada953ffb9e22ad16bea
RaffaeleParadiso <raffaele05@gmail.com>
2023-03-09 18:04:33 +0100

    error approx log, normal and plot

--- cal_theory/COS_LogNormal_Density_Recovery.py
+++ cal_theory/COS_LogNormal_Density_Recovery.py
@@ -1,10 +1,8 @@
 """
 Normal and LogNormal density recovery using the COS method
 """
-
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as st
+
 
 def COSDensity(cf,x,N,a,b):
     i = complex(0.0,1.0)
@@ -17,26 +15,45 @@ def COSDensity(cf,x,N,a,b):
     return f_X
     
 if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+    from matplotlib import pylab
+    import scipy.stats as st
+    from scipy.stats import lognorm
+    from pylab import *
+    pylab.rcParams['figure.figsize'] = (6.5, 4)
+
+    SAVE = 1
     i = complex(0.0, 1.0)
     a = -10
     b = 10
-    N = [16, 64, 128]
-    mu = 0.5
-    sigma = 0.2  
+    N = [2**x for x in range(2,9,1)]
+    mu = 0
+    sigma = 0.25  
     cF = lambda u : np.exp(i * mu * u - 0.5 * np.power(sigma,2.0) * np.power(u,2.0))
-    y = np.linspace(0.05,5,1000) 
+    y = np.linspace(0.05,5,1000)
+
+    dist=lognorm([sigma],loc=mu)
+    ff = dist.pdf(y)
 
     plt.figure()
     plt.grid()
     plt.xlabel("y")
     plt.ylabel("$f_Y(y)$")
+    error_list0 = [] 
     for n in N:
         f_Y = 1/y * COSDensity(cF,np.log(y),n,a,b)
-        plt.plot(y,f_Y)
-    plt.legend(["N=%.0f"%N[0],"N=%.0f"%N[1],"N=%.0f"%N[2]])
+        error = np.max(np.abs(f_Y-ff))
+        error_list0.append([n,error])
+        print("For {0} expansion terms the error is {1}".format(n,error))
+        plt.plot(y,f_Y, label=f"{n}")
+    plt.plot(y,dist.pdf(y),'x',markersize=2, label="Exact")
+    plt.legend()#["N=%.0f"%i for i in N])
+    plt.title(rf"Log Normal Density recover with ${{\mu}}={mu}$ and ${{\sigma}} = {sigma}$")
+    if SAVE: plt.savefig("Log_COS.png",bbox_inches='tight')
+    np.savetxt("error_log_normal_approx_cos_method.txt", error_list0, fmt=['%.0i','%.18e'])
     plt.show()
 
-    N = [2**x for x in range(2,7,1)]
     mu = 0.0
     sigma = 1.0
     x = np.linspace(-10.0,10,1000)
@@ -46,9 +63,16 @@ if __name__ == "__main__":
     plt.grid()
     plt.xlabel("x")
     plt.ylabel("$f_X(x)$")
+    error_list = [] 
     for n in N:
         f_X = COSDensity(cF,x,n,a,b)
         error = np.max(np.abs(f_X-f_XExact))
-        print("For {0} expanansion terms the error is {1}".format(n,error))
-        plt.plot(x,f_X)
+        error_list.append([n,error])
+        print("For {0} expansion terms the error is {1}".format(n,error))
+        plt.plot(x,f_X, label=f"{n}")
+    plt.plot(x, f_XExact, 'x',markersize=2, label="Exact")
+    plt.legend()
+    plt.title(rf"Normal Density recover with ${{\mu}}={mu}$ and ${{\sigma}} = {sigma}$")
+    if SAVE: plt.savefig("Normal_COS.png",bbox_inches='tight')
     plt.show()
+    np.savetxt("error_normal_approx_cos_method.txt", error_list, fmt=['%.0i','%.18e'])
